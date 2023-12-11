5467483c920dcb20c1286832872898a4dbb0d4a2
Saran <46473540+sarannetworkprogammer@users.noreply.github.com>
2023-12-11 14:03:40 -0600

    Update first.py

--- challenge/first.py
+++ challenge/first.py
@@ -1,1 +1,17 @@
+import streamlit as st
+
+st.title("60_Day_Coding_Challenge")
+
+
+with st.container():
+  st.header("Day_2 Dec-12")
+
+with st.container():
+  st.header("Day_1 Dec-11")
+
+  with st.container():
+    st.write("1. Hosting this website using github and streamlit cloud")
+    st.write("2. completion of sql queries joins and aggregates")
+    st.write("3. leet code question island question")
+
 
