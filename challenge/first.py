855c0c77bd99936204a4fc98f275792635ba69f9
saran <saranchowdari@gmail.com>
2023-12-14 09:12:38 -0600

    functions

--- challenge/first.py
+++ challenge/first.py
@@ -3,21 +3,34 @@ import streamlit as st
 st.title("60_Day_Coding_Challenge")
 
 
+with st.contao
 
-with st.container():
-  st.header("Day_2 Dec-12")
-  
-  with st.container():
-    st.write("1. Solving leet code questions")
-    st.text("2. Done mock interview with friend, suggestion to improvise on edge cases")
-    st.text("3. Finished SQL queries on CRUD operations")
 
-with st.container():
-  st.header("Day_1 Dec-11")
+def day2():
 
   with st.container():
-    st.text("1. Hosting this website using github and streamlit cloud")
-    st.text("2. completion of sql queries joins and aggregates")
-    st.text("3. leet code question island question")
-   
+    st.header("Day_2 Dec-12")
+    
+    with st.container():
+      st.write("1. Solving leet code questions")
+      st.text("2. Done mock interview with friend, suggestion to improvise on edge cases")
+      st.text("3. Finished SQL queries on CRUD operations")
 
+def day1():
+
+  with st.container():
+    st.header("Day_1 Dec-11")
+
+    with st.container():
+      st.text("1. Hosting this website using github and streamlit cloud")
+      st.text("2. completion of sql queries joins and aggregates")
+      st.text("3. leet code question island question")
+    
+
+def main ():
+  day2()
+  day1()
+
+
+if __name__ == "__main__":
+  main()
